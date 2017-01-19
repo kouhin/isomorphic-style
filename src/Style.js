cdd7979867196581a4a8705c7ac983ff2e0eccc7
kou_hin <kou_hin@cyberagent.co.jp>
2017-01-19 16:08:46 +0900

    make default children in <Style /> to null

--- src/Style.js
+++ src/Style.js
@@ -2,7 +2,29 @@
 import React from 'react';
 import { mountStyle, unmountStyle } from './helpers';
 
-class Style extends React.Component {
+export default class Style extends React.Component {
+
+  static propTypes = {
+    children: React.PropTypes.node,
+    style: React.PropTypes.oneOfType([
+      React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          insertCss: React.PropTypes.func.isRequired,
+          removeCss: React.PropTypes.func.isRequired,
+        }),
+      ),
+      React.PropTypes.shape({
+        insertCss: React.PropTypes.func.isRequired,
+        removeCss: React.PropTypes.func.isRequired,
+      }),
+    ]),
+  };
+
+  static get defaultProps() {
+    return {
+      children: null,
+    };
+  }
 
   componentWillMount() {
     mountStyle(this.props.style);
@@ -16,21 +38,3 @@ class Style extends React.Component {
     return this.props.children;
   }
 }
-
-Style.propTypes = {
-  children: React.PropTypes.node,
-  style: React.PropTypes.oneOfType([
-    React.PropTypes.arrayOf(
-      React.PropTypes.shape({
-        insertCss: React.PropTypes.func.isRequired,
-        removeCss: React.PropTypes.func.isRequired,
-      }),
-    ),
-    React.PropTypes.shape({
-      insertCss: React.PropTypes.func.isRequired,
-      removeCss: React.PropTypes.func.isRequired,
-    }),
-  ]),
-};
-
-export default Style;
